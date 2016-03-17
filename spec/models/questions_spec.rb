56cc29d2822fb66345f367642c6f7f4d9ff6892b
nebtrx <neb.trx@gmail.com>
2016-03-17 14:06:13 -0500

    Add one more specs for Question model

--- spec/models/questions_spec.rb
+++ spec/models/questions_spec.rb
@@ -2,10 +2,19 @@ require_relative '../spec_helper'
 
 describe Question do
   describe '.find_next' do
-    it "gets the next question's answer" do
+    context 'while there is stil questions in the survey' do
+      it "returns the next question's answer" do
 
-      next_question = Question.find_next(1)
-      expect(next_question.body).to include('0 to 9')
+        next_question = Question.find_next(1)
+        expect(next_question.body).to include('0 to 9')
+      end
+    end
+    context "while there is isn't questions in the survey" do
+      it "return null" do
+
+        next_question = Question.find_next(4)
+        expect(next_question).to be_nil
+      end
     end
   end
 end
