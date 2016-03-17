dba6f1ba23ed7b297b5f568bf37c62c05fe5aa87
nebtrx <neb.trx@gmail.com>
2016-03-17 15:55:29 -0500

    Add missing changes for 'answers_specs.rb'

--- spec/app/answers_spec.rb
+++ spec/app/answers_spec.rb
@@ -1,37 +1,74 @@
 require_relative '../spec_helper'
 
 describe 'POST /questions/:question_id/answers' do
-  it "should create ...." do
-    answer = Answer.new(
-      recording_url: 'http://example.com',
-      digits: '1',
-      call_sid: 'CS2222',
-      from: '5555555'
-    )
-    question = Question.get(2)
-
-    expect(Answer).to receive(:create)
-      .with(hash_including(
+  context 'while there is still questions in the survey' do
+    it "should create a new answer and return TwiML for the second question" do
+      answer = Answer.new(
         recording_url: 'http://example.com',
         digits: '1',
         call_sid: 'CS2222',
         from: '5555555',
-        question_id: 1))
-      .and_return(answer)
+        question_id: 1
+      )
+      question = Question.get(2)
 
-    expect(Question).to receive(:find_next)
-      .with(1)
-      .and_return(question)
+      expect(Answer).to receive(:create)
+        .with(hash_including(
+          recording_url: 'http://example.com',
+          digits: '1',
+          call_sid: 'CS2222',
+          from: '5555555',
+          question_id: 1))
+        .and_return(answer)
 
-    expect(TwimlGenerator).to receive(:generate_for_question)
-      .with(question)
-      .once
-      .and_return('TwiML')
+      expect(Question).to receive(:find_next)
+        .with(1)
+        .and_return(question)
 
-    post '/questions/1/answers', recording_url: 'http://example.com', digits: '1',
-      call_sid: 'CS2222', from: '5555555'
+      expect(TwimlGenerator).to receive(:generate_for_question)
+        .once
+        .and_return('TwiML')
 
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('TwiML')
+      post '/questions/1/answers', recording_url: 'http://example.com', digits: '1',
+        call_sid: 'CS2222', from: '5555555'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('TwiML')
+    end
+  end
+
+  context "while there there isn't questions in the survey" do
+    it "should create a new answer and return TwiML for exiting the survey" do
+      answer = Answer.new(
+        recording_url: 'http://example.com',
+        digits: '1',
+        call_sid: 'CS2222',
+        from: '5555555',
+        question_id: 4
+      )
+
+      expect(Answer).to receive(:create)
+        .with(hash_including(
+          recording_url: 'http://example.com',
+          digits: '1',
+          call_sid: 'CS2222',
+          from: '5555555',
+          question_id: 4))
+        .and_return(answer)
+
+      expect(Question).to receive(:find_next)
+        .with(4)
+        .and_return(nil)
+
+      expect(TwimlGenerator).to receive(:generate_for_exit)
+        .once
+        .and_return('TwiML')
+
+      post '/questions/4/answers', recording_url: 'http://example.com', digits: '1',
+        call_sid: 'CS2222', from: '5555555'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('TwiML')
+    end
   end
 end
